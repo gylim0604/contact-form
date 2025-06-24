620af882884befb7ad957d2f17c43134acb80a51
gylim0604 <gylim0604@hotmail.com>
2025-06-24 19:27:04 +0800

    add error handling for schema

--- front-end/src/QueryForm/form.test.tsx
+++ front-end/src/QueryForm/form.test.tsx
@@ -13,4 +13,25 @@ describe('form schema', () => {
 		});
 		expect(result.success).toBe(true);
 	});
+
+	it('fails with empty fields', () => {
+		const result = formSchema.safeParse({
+			firstName: '',
+			lastName: '',
+			email: '',
+			queryType: '',
+			message: '',
+			checkbox: false,
+		});
+		expect(result.success).toBe(false);
+		if (!result.success) {
+			const errors = result.error.format();
+			expect(errors.firstName?._errors[0]).toEqual('This field is required');
+			expect(errors.lastName?._errors[0]).toEqual('This field is required');
+			expect(errors.email?._errors[0]).toEqual('Please enter a valid email address');
+			expect(errors.queryType?._errors[0]).toEqual('Please select a query type');
+			expect(errors.message?._errors[0]).toEqual('This field is required');
+			expect(errors.checkbox?._errors[0]).toEqual('To submit this form, please consent to being contacted');
+		}
+	});
 });
--- front-end/src/QueryForm/index.tsx
+++ front-end/src/QueryForm/index.tsx
@@ -1,12 +1,14 @@
 import { z } from 'zod';
 
 export const formSchema = z.object({
-	firstName: z.string().min(1),
-	lastName: z.string().min(1),
-	email: z.string().min(1),
-	queryType: z.string().min(1),
-	message: z.string().min(1),
-	checkbox: z.boolean(),
+	firstName: z.string().min(1, { message: 'This field is required' }),
+	lastName: z.string().min(1, { message: 'This field is required' }),
+	email: z.string().min(1, { message: 'Please enter a valid email address' }),
+	queryType: z.string().min(1, { message: 'Please select a query type' }),
+	message: z.string().min(1, { message: 'This field is required' }),
+	checkbox: z.literal(true, {
+		errorMap: () => ({ message: 'To submit this form, please consent to being contacted' }),
+	}),
 });
 
 export function QueryForm() {
